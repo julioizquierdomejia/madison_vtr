ffd0e85f231104d71e1a2cdbea5c57e784d373ef
Julio Izquierdo Mejia <julioizquierdomejia@Julios-MacBook-Pro.local>
2020-11-27 10:46:33 -0500

    a devel

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::get('/rituales', [App\Http\Controllers\RitualController::class, 'index'])->name('ritual');
 Route::get('/', [App\Http\Controllers\RitualController::class, 'index'])->name('ritual');
 
