b5c5802c4783e9f15d4673203000283d7ca97ca5
Miguel AB <abm_8249@outlook.com>
2020-12-16 22:15:01 -0500

    corregir select de rituales

--- app/Http/Controllers/RitualController.php
+++ app/Http/Controllers/RitualController.php
@@ -32,8 +32,7 @@ class RitualController extends Controller
         $objectives = Objective::where('enabled', 1)->get();
         $rituales = Ritual::join('ritual_status', 'ritual_status.id', '=', 'rituals.ritual_status_id')
             ->join('ritual_types', 'ritual_types.id', '=', 'rituals.ritual_type_id')
-            ->join('ritual_objectives', 'ritual_objectives.id', '=', 'rituals.ritual_objective_id')
-            ->select('rituals.*', 'ritual_status.name as status', 'rituals.ritual_status_id', 'ritual_objectives.name as objective');
+            ->select('rituals.*', 'ritual_status.name as status', 'rituals.ritual_status_id');
         $status = RitualStatus::all();
 
         return view('admin.rituales.index', compact('rituales', 'objectives', 'status', 'ritual_types'));
